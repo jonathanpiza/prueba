f81913294de1d753f6cbd2ed96f817a66b1c5591
Jonathan Piza <yonathanpiza28@gmail.com>
2023-10-01 17:16:03 -0500

    visual

---
+++ funciones.js
@@ -0,0 +1,27 @@
+
+let array = [12, 12];
+write_table()
+function add(){
+    const id = document.getElementById("id").value;
+    const name = document.getElementById("name").value;
+    const cant = document.getElementById("cant").value;
+    const price = document.getElementById("price").value;
+    const desc = document.getElementById("desc").value;
+
+}
+
+function write_table(){
+    let resultado = document.getElementById("res")
+    console.log(resultado)
+    // let result = "";
+    // for (let index = 0; index < array.length; index++) {
+    //     result+="<tr>";
+    //     result+="<td>1234</td>";
+    //     result+="</tr>"
+    // }
+    
+}
+
+function remove(id){
+
+}
