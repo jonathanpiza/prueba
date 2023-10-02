b468ffec31675177c34378028961c8b17895941d
jonathanpiza <yonathanpiza28@gmail.com>
2023-10-02 09:28:23 -0500

    verificacion input agg y eliminar

--- funciones.js
+++ funciones.js
@@ -1,27 +1,81 @@
 
-let array = [12, 12];
-write_table()
-function add(){
-    const id = document.getElementById("id").value;
-    const name = document.getElementById("name").value;
-    const cant = document.getElementById("cant").value;
-    const price = document.getElementById("price").value;
-    const desc = document.getElementById("desc").value;
+let array = [];
 
+write_table()
+
+function add(){
+    const id = document.getElementById("id");
+    const name = document.getElementById("name");
+    const cant = document.getElementById("cant");
+    const price = document.getElementById("price");
+    const desc = document.getElementById("desc");
+    if(id.value==""){
+        id.className="textrecperr"
+    }else{
+        id.className="textrecp"
+    }
+
+    if(name.value==""){
+        name.className="textrecperr"
+    }else{
+        name.className="textrecp"
+    }
+
+    if(cant.value==""){
+        cant.className="textrecperr"
+    }else{
+        cant.className="textrecp"
+    }
+
+    if(price.value==""){
+        price.className="textrecperr"
+    }else{
+        price.className="textrecp"
+    }
+
+    if(desc.value==""){
+        desc.className="textrecperr"
+    }else{
+        desc.className="textrecp"
+    }
+
+
+    if(id.value!=""&&name.value!=""&&cant.value!=""&&price.value!=""&&desc.value!=""){
+        const objeto = {
+            id: id.value,
+            name: name.value,
+            cant: cant.value,
+            price: price.value,
+            desc: desc.value
+        }
+        array.push(objeto)
+        write_table()
+        id.value=""
+        name.value=""
+        cant.value=""
+        price.value=""
+        desc.value=""
+    }
+    
 }
 
 function write_table(){
     let resultado = document.getElementById("res")
-    console.log(resultado)
-    // let result = "";
-    // for (let index = 0; index < array.length; index++) {
-    //     result+="<tr>";
-    //     result+="<td>1234</td>";
-    //     result+="</tr>"
-    // }
-    
+    let result = "";
+    for (let index = 0; index < array.length; index++) {
+        result+="<tr>";
+        result+="<td>"+array[index].id+"</td>";
+        result+="<td>"+array[index].name+"</td>";
+        result+="<td>"+array[index].cant+"</td>";
+        result+="<td>"+array[index].price+"</td>";
+        result+="<td>"+array[index].desc+"</td>";
+        result+="<td><button class='remove' onclick='remove("+index+")'>Borrar</button></td>";
+        result+="</tr>"
+    }
+    resultado.innerHTML=result
 }
 
 function remove(id){
-
+    array.splice(id, 1)
+    write_table()
 }
